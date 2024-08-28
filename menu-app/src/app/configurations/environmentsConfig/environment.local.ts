28a8030a355c4e7eb3e2d2ec1e58b4bf6a2d7eb8
Mohammad Daoud <m.a.daoud99@gmail.com>
2024-08-28 14:20:57 +0300

    Implements log tracking and modifies the backend port to 9090

--- menu-app/src/app/configurations/environmentsConfig/environment.local.ts
+++ menu-app/src/app/configurations/environmentsConfig/environment.local.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/v1/products/'  // Change to your development URL
+    apiUrl: 'http://localhost:9090/api/v1/products/'  // Change to your development URL
 };
