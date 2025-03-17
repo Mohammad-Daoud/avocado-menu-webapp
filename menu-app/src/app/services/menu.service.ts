e220d42605f44ea111320193ecfc67f8be773f5f
Mohammad-Daoud <m.a.daoud99@gmail.com>
2025-03-18 02:42:40 +0300

    make the data came from static file

--- menu-app/src/app/services/menu.service.ts
+++ menu-app/src/app/services/menu.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import { environment } from '../configurations/environmentsConfig/environment.local'
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,19 @@ export class MenuService {
   private apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
-  getMenu(target:string): Observable<any> {
-    return this.http.get<any>(this.apiUrl+target);
+/*  getMenu(target: string): Observable<any> {
+    const staticData = {
+      "menu1": { "id": 1, "name": "Menu 1", "items": ["Item 1", "Item 2"] },
+      "menu2": { "id": 2, "name": "Menu 2", "items": ["Item A", "Item B"] }
+    };
+    return new Observable(observer => {
+      observer.next(staticData[target] || {});
+      observer.complete();
+    });
+  }*/
+  getMenu(target: string): Observable<any> {
+    return this.http.get<any>('assets/menu-data.json').pipe(
+        map(data => data[target] || {})
+    );
   }
 }
